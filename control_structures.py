01a5197b2f003c43e77aeba0d38bbeb9d022b9d0
Obadha <wobadha@gmail.com>
2016-03-22 14:00:43 +0300

    Control Structures and Function examples

---
+++ control_structures.py
@@ -0,0 +1,24 @@
+# if False:
+# 	print "it's true"
+# else:
+#	print "it's false"
+
+# if 2>6:
+# 	print "You're awesome"
+# elif 4<6:
+# 	print "Yes sir!"
+# else:
+# 	print "Okay Maybe Not"
+
+# for i in xrange (10):
+#   if i % 2:
+# 	   print i,
+# find out if divisible by 3 and 5
+
+# counter = 0
+# while counter < 5:
+# 	print "its true"
+# 	print counter
+# 	counter = counter + 1
+
+
---
+++ functions.py
@@ -0,0 +1,10 @@
+# def say_my_name(name):
+# 	print "name? No need for {}".format(name)
+# # call the function called say_my_name
+# say_my_name("los")
+
+def add_two_numbers (number1,number2):
+	total = number1 + number2
+	return total
+a = add_two_numbers(2,3)
+print a
